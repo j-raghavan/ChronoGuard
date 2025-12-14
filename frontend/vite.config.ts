1d56c850ee002f269554c1afef88d4b16184b377
Jayasimha Raghavan <mr.jayasimha@gmail.com>
2025-12-13 17:17:49 -0800

    chore: fixed vite base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages repo name
+  base: process.env.NODE_ENV === "production" ? "/ChronoGuard/" : "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
